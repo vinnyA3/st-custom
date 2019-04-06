3d8546e685211b8c2f80841aa31c83c79e14c85c
vinnyA3 <vincent.aceto@gmail.com>
2019-04-06 13:32:03 -0400

    feat(config): update lh and border px

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Tamzen:pixelsize=13:antialias=true:autohint=true";
-static int borderpx = 10;
+static int borderpx = 8;
 
 /*
  * What program is execed by st depends of these precedence rules:
